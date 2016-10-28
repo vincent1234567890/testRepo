8837ae6f400364f5de9fb3bd9cbc94cbb33f23a4
Eugene Seah <Eugene.Seah@sinonet.sg>
2016-10-28 15:56:17 +0800

    Resolution change + multiplayer cannons

--- main.js
+++ main.js
@@ -59,7 +59,7 @@ var cocos2dApp = cc.game.onStart = function() {
     // cc.view.setOrientation(cc.ORIENTATION_PORTRAIT);
 
     // Setup the resolution policy and design resolution size
-    cc.view.setDesignResolutionSize(1400, 980, cc.ResolutionPolicy.SHOW_ALL);
+    cc.view.setDesignResolutionSize(1365, 768, cc.ResolutionPolicy.SHOW_ALL);
 
     // The game will be resized when browser size change
     cc.view.resizeWithBrowserSize(true);
--- src/New/GameManager.js
+++ src/New/GameManager.js
@@ -13,13 +13,16 @@
 var GameManager = function(){
     // var _gameView;
     var _gameConfig;
+    var _playerSlot;
     var _playerId;
     var _parentNode;
 
-    var _topLeft;
-    var _topRight;
-    var _bottomLeft;
-    var _bottomRight;
+    var _playerPositions
+
+    // var _topLeft;
+    // var _topRight;
+    // var _bottomLeft;
+    // var _bottomRight;
 
     //production
     // var _other;
@@ -34,13 +37,13 @@ var GameManager = function(){
     //bad : refactor touchlayer
     var controlNewPosition = function (control, pos, yPos) {
         // change this to current player position
-        var rot = this._bottomLeft.turnTo(pos);
+        var rot = _playerPositions[_playerSlot].turnTo(pos);
         const bulletId = _playerId + ':' + getPlayerBulletId();
         GameCtrl.informServer.bulletFired(bulletId, rot);
     }
 
     var getPlayerBulletId = function(){
-        return GameManager._bottomLeft.getNextBulletId();
+        return _playerPositions[_playerSlot].getNextBulletId();
     }
 
     var initialise = function (parent) {
@@ -59,34 +62,42 @@ var GameManager = function(){
         // _gameConfig.cannonPositions[2] = [125,980-56];
         // _gameConfig.cannonPositions[3] = [1400 - 125,980-56];
 
+        _playerPositions = [];
+
+        for (var i = 0; i < _gameConfig.maxPlayers ; i++){
+            _playerPositions[i] = new PlayerViewManager(_parentNode, _gameConfig.cannonPositions, i);
+        }
+
+        // for (var p of this._playerPositions ){
+        //
+        // }
 
 
-        this._bottomLeft = new PlayerViewManager(_parentNode, _gameConfig.cannonPositions, 0);
-        this._bottomRight = new PlayerViewManager(_parentNode, _gameConfig.cannonPositions, 1);
-        this._topLeft = new PlayerViewManager(_parentNode, _gameConfig.cannonPositions, 2);
-        this._topRight = new PlayerViewManager(_parentNode, _gameConfig.cannonPositions, 3);
+        // this._bottomLeft = new PlayerViewManager(_parentNode, _gameConfig.cannonPositions, 0);
+        // this._bottomRight = new PlayerViewManager(_parentNode, _gameConfig.cannonPositions, 1);
+        // this._topLeft = new PlayerViewManager(_parentNode, _gameConfig.cannonPositions, 2);
+        // this._topRight = new PlayerViewManager(_parentNode, _gameConfig.cannonPositions, 3);
 
         // this._cannonManager = new CannonManager(this._playerViewStaticPrefab, cc.p(150,60));
         initialiseTouch(this);
     }
 
     var shootTo = function(pos){
-        return this._bottomLeft.shootTo(pos);
+        return _playerPositions[_playerSlot].shootTo(pos);
     }
 
-    var setGameConfig = function (config) {
+    var setGameState = function (config, playerId, playerSlot) {
         console.log(JSON.stringify(config));
         _gameConfig = config;
+        _playerId = playerId;
+        _playerSlot = playerSlot;
     }
 
-    var setPlayerId = function (playerId) {
-        _playerId = playerId;
-    }
 
     var GameManager = {
         initialise : initialise,
-        setGameConfig : setGameConfig,
-        setPlayerId : setPlayerId,
+        setGameState : setGameState,
+        // setPlayerId : setPlayerId,
         shootTo : shootTo, //sliohtly unsatisfactory
 
         //change to callback strategy instead of reverse public call
