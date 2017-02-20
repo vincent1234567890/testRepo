305e7780028cb74cf78f4d738b1fb4829e0d4c1b
Eugene Seah <eugene.seah@sinonet.sg>
2017-02-20 18:47:42 +0800

    fixes for fishes and lantern plist

--- src/New/Arena/BulletManager.js
+++ src/New/Arena/BulletManager.js
@@ -22,7 +22,7 @@ const BulletManager = (function(){
     const proto = BulletManager.prototype;
 
     proto.createBullet = function (gunId, bulletId) {
-        if (gunId >5) gunId = 4;
+        if (gunId >5) gunId = 4; // temp fix
         _bulletCache[bulletId] = new BulletView(_parent, "#Bullet"+(gunId+1)+".png");
         // console.log(_bulletCache);
     };
