09baf18b44d67881e0b6e83f19b3701d380c8295
Eugene Seah <eugene.seah@sinonet.sg>
2017-02-20 17:14:53 +0800

    fix for gunId > 4

--- src/New/Arena/BulletManager.js
+++ src/New/Arena/BulletManager.js
@@ -22,6 +22,7 @@ const BulletManager = (function(){
     const proto = BulletManager.prototype;
 
     proto.createBullet = function (gunId, bulletId) {
+        if (gunId >5) gunId = 4;
         _bulletCache[bulletId] = new BulletView(_parent, "#Bullet"+(gunId+1)+".png");
         // console.log(_bulletCache);
     };
