23a395aaa5868d659b989bf0d693c57eff8d0f36
Eugene Seah <eugene.seah@sinonet.sg>
2017-04-11 15:41:10 +0800

    Floating menu Profile View update

--- src/New/Static Views/ProfileView.js
+++ src/New/Static Views/ProfileView.js
@@ -4,27 +4,13 @@
 
 const ProfileView = (function () {
     "use strict";
-    const STARTOFFSET = 0.03;
-    const ENDOFFSET = 0.05;
     const ZORDER = 10;
-    const STARTING_ALIGNMENT = 175;
+    const startHeight = 300;
+    const LeftColumn = 250;
+    const RightColumn = 550;
+    const Spacing = 100;
     const ProfileView = function () {
-        /*
-         SettingsBackground : "#PopupBase1.png",
-         SettingsTitleChinese : "#SettingChineseTitle.png",
-         SettingsTitleBackground : "#TitleBase1.png",
-         SettingsButtonBackground : "#Button1Idle.png",
-         SettingsButtonBackgroundOnPress : "#Button1OnPress.png",
-         SettingsSliderBackground : "#BarBase1.png",
-         SettingsSliderFiller : "#BarMid1.png",
-         SettingsSliderIndicator : "#Puller.png",
-         SettingsMusicTitleChinese : "#BGMChinese.png",
-         SettingsSoundTitleChinese : "#MusicFXChinese.png",
-         SettingsGameLanguageSelectionTitleChinese : "#GameLangChinese.png",
-         SettingsGameLanguageSelectionBar : "#BarBase2.png",
-         SettingsDropDownButton : "DropDWIdle.png",
-         SettingsDropDownButtonOnPress : "DropDWOnPress.png",
-         */
+
         const parent = new cc.Node();
         const _background = new cc.Sprite(ReferenceName.SettingsBackground);
         const _closeButton = new CloseButtonPrefab(dismiss);
@@ -34,43 +20,23 @@ const ProfileView = (function () {
 
         parent.setPosition(683, 384);
 
-        // const _musicSlider = createSlider(musicValueChangedEvent, PlayerPreferences.getMusicVolume());
-        // const _soundSlider = createSlider(soundValueChangedEvent, PlayerPreferences.getSoundVolume());
-        //
-        // _background.addChild(_musicSlider);
-        // _background.addChild(_soundSlider);
-        //
-        // const musicSliderHeight = 350;
-        // const soundSliderHeight = 270;
-        // const gameLanguageSelectionHeight = 190;
-        //
-        // _musicSlider.setPosition(475, musicSliderHeight);
-        // _soundSlider.setPosition(475, soundSliderHeight);
+        const timeDisplay = createDisplay(ReferenceName.ProfileTimeIcon,"timeTitle", "timeText");
+        const coinDisplay = createDisplay(ReferenceName.ProfileCoinIcon,"coinTitle", "coinText");
+        const rankingDisplay = createDisplay(ReferenceName.ProfileRankingIcon,"rankingTitle", "rankingText");
+        const coinStackDisplay = createDisplay(ReferenceName.ProfileCoinStackIcon,"coinStackTitle", "coinStackText");
 
-        const timeIcon = new cc.Sprite(ReferenceName.ProfileTimeIcon);
-        const coinIcon = new cc.Sprite(ReferenceName.CoinIcon);
-        const rankingIcon = new cc.Sprite(ReferenceName.FAQFishInfoButtonText);
-        const coinStack = new cc.Sprite(ReferenceName.FAQCannonInfoButtonText);
-        const jackpotInfoButtonText = new cc.Sprite(ReferenceName.FAQJackpotInfoButtonText);
+        _background.addChild(coinDisplay);
+        _background.addChild(timeDisplay);
+        _background.addChild(rankingDisplay);
+        _background.addChild(coinStackDisplay);
 
-        const pos = new cc.p(gameRules.getContentSize().width/2, gameRules.getContentSize().height/2);
+        timeDisplay.setPosition(LeftColumn,startHeight - Spacing);
+        coinDisplay.setPosition(LeftColumn,startHeight - Spacing*2);
+        rankingDisplay.setPosition(RightColumn,startHeight - Spacing);
+        coinStackDisplay.setPosition(RightColumn,startHeight - Spacing*2);
 
-        console.log(gameRules,gameRules.getContentSize(),pos);
-
-        gameRulesButtonText.setPosition(pos);
-        uiFAQButtonText.setPosition(pos);
-        fishInfoButtonText.setPosition(pos);
-        cannonInfoButtonText.setPosition(pos);
-        jackpotInfoButtonText.setPosition(pos);
-
-        gameRules.addChild(gameRulesButtonText);
-        uiFAQ.addChild(uiFAQButtonText);
-        fishInfo.addChild(fishInfoButtonText);
-        cannonInfo.addChild(cannonInfoButtonText);
-        jackpotInfo.addChild(jackpotInfoButtonText);
-
-
-        var textName = new cc.EditBox(cc.size(300,70), ReferenceName.Profile);
+        //------
+        var textName = new cc.EditBox(cc.size(300,70), ReferenceName.ProfileUserTextBackground);
         textName.setPlaceHolder(ReferenceName.LoginNamePlaceHolder);
         textName.setPosition(midX, midY + 100);
         textName.setFontSize(40);
@@ -79,81 +45,66 @@ const ProfileView = (function () {
         textName.needsLayout();
         this._parent.addChild(textName);
         this.textName = textName;
+//-------
 
-        const title = new cc.Sprite(ReferenceName.SettingsTitleChinese);
+
+
+
+
+        const title = new cc.Sprite(ReferenceName.ProfileTitleChinese);
         const titleBackground = new cc.Sprite(ReferenceName.SettingsTitleBackground);
-        const musicTitle = new cc.Sprite(ReferenceName.SettingsMusicTitleChinese);
-        const soundTitle = new cc.Sprite(ReferenceName.SettingsSoundTitleChinese);
-        const gameLanguageSelectionTitle = new cc.Sprite(ReferenceName.SettingsGameLanguageSelectionTitleChinese);
-        const gameLanguageSelectionBar = new cc.Sprite(ReferenceName.SettingsGameLanguageSelectionBar);
 
         const titlePosition = new cc.p(400, 450);
         title.setPosition(titlePosition);
         titleBackground.setPosition(titlePosition);
-
-        musicTitle.setPosition(STARTING_ALIGNMENT, musicSliderHeight);
-        soundTitle.setPosition(STARTING_ALIGNMENT, soundSliderHeight);
-
-        gameLanguageSelectionTitle.setPosition(STARTING_ALIGNMENT, gameLanguageSelectionHeight);
-        gameLanguageSelectionBar.setPosition(400, gameLanguageSelectionHeight);
-
-        let fontDef = new cc.FontDefinition();
-        fontDef.fontName = "Microsoft YaHei";
-        // fontDef.fontName = "Arial Unicode MS";
-        fontDef.fontSize = "20";
-        fontDef.fontStyle = "bold";
-        fontDef.textAlign = cc.TEXT_ALIGNMENT_LEFT;
-        fontDef.fillStyle = new cc.Color(255, 255, 255, 255);
-        let label = new cc.LabelTTF("中文", fontDef);
-        label.setPosition(300, gameLanguageSelectionHeight);
-
-        const acceptButton = GUIFunctions.createButton(ReferenceName.SettingsButtonBackground, ReferenceName.SettingsButtonBackgroundOnPress, dismiss);
-        const cancelButton = GUIFunctions.createButton(ReferenceName.SettingsButtonBackground, ReferenceName.SettingsButtonBackgroundOnPress, cancel);
-
-        const buttonLevel = 80;
-
-        acceptButton.setPosition(250, buttonLevel);
-        cancelButton.setPosition(550, buttonLevel);
-
-        const acceptText = new cc.Sprite(ReferenceName.SettingsConfirmButtonTextChinese);
-        const cancelText = new cc.Sprite(ReferenceName.SettingsCancelButtonTextChinese);
-
-        acceptText.setPosition(acceptButton.getPosition());
-        cancelText.setPosition(cancelButton.getPosition());
-
-        // const touchEvent = (sender, type) => {
-        //     switch (type) {
-        //         case ccui.Widget.TOUCH_ENDED:
-        //             break;
-        //     }
-        // };
         //
-        // const dropDownButton = new ccui.Button(ReferenceName.SettingsDropDownButton, ReferenceName.SettingsDropDownButtonOnPress, undefined, ccui.Widget.PLIST_TEXTURE);
-        // dropDownButton.setTouchEnabled(true);
-        // dropDownButton.setPosition(gameLanguageSelectionBar.getContentSize().width, gameLanguageSelectionBar.getContentSize().height / 2);
-        // dropDownButton.addTouchEventListener(touchEvent);
+        // musicTitle.setPosition(STARTING_ALIGNMENT, musicSliderHeight);
+        // soundTitle.setPosition(STARTING_ALIGNMENT, soundSliderHeight);
         //
-        // gameLanguageSelectionBar.addChild(dropDownButton);
-
-        _background.addChild(titleBackground);
-        _background.addChild(title);
-        _background.addChild(musicTitle);
-        _background.addChild(soundTitle);
-        _background.addChild(gameLanguageSelectionTitle);
-        _background.addChild(gameLanguageSelectionBar);Ø
-        _background.addChild(label);
-        _background.addChild(acceptButton);
-        _background.addChild(cancelButton);
-        _background.addChild(acceptText);
-        _background.addChild(cancelText);
+        // gameLanguageSelectionTitle.setPosition(STARTING_ALIGNMENT, gameLanguageSelectionHeight);
+        // gameLanguageSelectionBar.setPosition(400, gameLanguageSelectionHeight);
+        //
+        // let fontDef = new cc.FontDefinition();
+        // fontDef.fontName = "Microsoft YaHei";
+        // // fontDef.fontName = "Arial Unicode MS";
+        // fontDef.fontSize = "20";
+        // fontDef.fontStyle = "bold";
+        // fontDef.textAlign = cc.TEXT_ALIGNMENT_LEFT;
+        // fontDef.fillStyle = new cc.Color(255, 255, 255, 255);
+        // let label = new cc.LabelTTF("中文", fontDef);
+        // label.setPosition(300, gameLanguageSelectionHeight);
+        //
+        // const acceptButton = GUIFunctions.createButton(ReferenceName.SettingsButtonBackground, ReferenceName.SettingsButtonBackgroundOnPress, dismiss);
+        // const cancelButton = GUIFunctions.createButton(ReferenceName.SettingsButtonBackground, ReferenceName.SettingsButtonBackgroundOnPress, cancel);
+        //
+        // const buttonLevel = 80;
+        //
+        // acceptButton.setPosition(250, buttonLevel);
+        // cancelButton.setPosition(550, buttonLevel);
+        //
+        // const acceptText = new cc.Sprite(ReferenceName.SettingsConfirmButtonTextChinese);
+        // const cancelText = new cc.Sprite(ReferenceName.SettingsCancelButtonTextChinese);
+        //
+        // acceptText.setPosition(acceptButton.getPosition());
+        // cancelText.setPosition(cancelButton.getPosition());
+        //
+        // _background.addChild(titleBackground);
+        // _background.addChild(title);
+        // _background.addChild(musicTitle);
+        // _background.addChild(soundTitle);
+        // _background.addChild(gameLanguageSelectionTitle);
+        // _background.addChild(gameLanguageSelectionBar);
+        // _background.addChild(label);
+        // _background.addChild(acceptButton);
+        // _background.addChild(cancelButton);
+        // _background.addChild(acceptText);
+        // _background.addChild(cancelText);
 
         parent.addChild(_background);
 
         GameView.addView(parent, 10);
         BlockingManager.registerBlock(dismissCallback);
 
-        let previousMusic = _musicSlider.getValue();
-        let previousSound = _soundSlider.getValue();
 
         function dismissCallback(touch) {
             if (GUIFunctions.isSpriteTouched(_background, touch)) {
@@ -163,12 +114,8 @@ const ProfileView = (function () {
         }
 
         this.show = function () {
-            previousMusic = _musicSlider.getValue();
-            previousSound = _soundSlider.getValue();
             parent.setLocalZOrder(ZORDER);
             _background.setVisible(true);
-            _musicSlider.setEnabled(true);
-            _soundSlider.setEnabled(true);
             _closeButton.setVisible(true);
 
             BlockingManager.registerBlock(dismissCallback);
@@ -179,27 +126,26 @@ const ProfileView = (function () {
         };
 
         function cancel() {
-            _musicSlider.setValue(previousMusic);
-            _soundSlider.setValue(previousSound);
             dismissCallback();
         }
 
         function dismiss() {
             parent.setLocalZOrder(-1000);
             _background.setVisible(false);
-            _musicSlider.setEnabled(false);
-            _soundSlider.setEnabled(false);
             _closeButton.setVisible(false);
-
-            previousMusic = -1;
-            previousSound = -1;
             BlockingManager.deregisterBlock(dismissCallback);
         }
 
         function createDisplay(spriteIcon, titleText, infoText){
             const iconBackground = new cc.Sprite(ReferenceName.ProfileIconBackground);
-            const background = new cc.Sprite(ReferenceName.ProfileBackground);
+            const background = new cc.Sprite(ReferenceName.ProfileIconTextBackground);
             const icon = new cc.Sprite(spriteIcon);
+
+            const iconPos = new cc.p(iconBackground.getContentSize().width/2, iconBackground.getContentSize().height/2);
+            const bgPos = new cc.p(background.getContentSize().width/2, background.getContentSize().height/2);
+            iconBackground.setPosition(iconPos.x,bgPos.y);
+            icon.setPosition(iconPos);
+
             // const soundTitle = new cc.Sprite(ReferenceName.SettingsSoundTitleChinese);
             let fontDef = new cc.FontDefinition();
             fontDef.fontName = "Microsoft YaHei";
@@ -207,9 +153,26 @@ const ProfileView = (function () {
             fontDef.fontSize = "20";
             fontDef.fontStyle = "bold";
             fontDef.textAlign = cc.TEXT_ALIGNMENT_LEFT;
+            fontDef.fillStyle = new cc.Color(0, 0, 0, 255);
+            let titleLabel = new cc.LabelTTF(titleText, fontDef);
+
+            titleLabel.setPosition(bgPos.x, bgPos.y + 20);
+            titleLabel.setAnchorPoint(0,0.5);
+
             fontDef.fillStyle = new cc.Color(255, 255, 255, 255);
-            let label = new cc.LabelTTF(titleText, fontDef);
+            let infoLabel = new cc.LabelTTF(titleText, fontDef);
+            infoLabel.setPosition(bgPos.x, bgPos.y - 20);
+            infoLabel.setAnchorPoint(0,0.5);
+
+            background.addChild(iconBackground);
+            iconBackground.addChild(icon);
+            background.addChild(titleLabel);
+            background.addChild(infoLabel);
+
+            return background;
         }
 
     };
+
+    return ProfileView;
 }());
